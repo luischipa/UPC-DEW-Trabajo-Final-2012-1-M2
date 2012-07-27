e63f2d9e2ad9141e6dc987d13620aa9787a3d2a8
max-felix <maxfelix1@gmail.com>
2012-07-27 11:32:50 -0500

    'validacioncampos'

--- veterinaria/app/models/client.rb
+++ veterinaria/app/models/client.rb
@@ -2,4 +2,9 @@ class Client < ActiveRecord::Base
 has_attached_file :avatar
 
 validates :name, :presence => true
+
+validates :email,   
+            :presence => true,   
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i }  
 end
