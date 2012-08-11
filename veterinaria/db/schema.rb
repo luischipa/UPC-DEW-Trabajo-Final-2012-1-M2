8f5eb19401740e44336669ee7ab9d7b2c1e2e9b4
luischipa <l.chipa.e@gmail.com>
2012-08-11 00:27:15 -0500

    login

--- veterinaria/db/schema.rb
+++ veterinaria/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120804042447) do
+ActiveRecord::Schema.define(:version => 20120811051424) do
 
   create_table "client_patients", :force => true do |t|
     t.integer  "client_id"
@@ -93,6 +93,15 @@ ActiveRecord::Schema.define(:version => 20120804042447) do
     t.string   "phonecelular"
     t.datetime "created_at",   :null => false
     t.datetime "updated_at",   :null => false
+  end
+
+  create_table "users", :force => true do |t|
+    t.string   "email"
+    t.string   "password_hash"
+    t.string   "password_salt"
+    t.datetime "created_at",    :null => false
+    t.datetime "updated_at",    :null => false
+    t.string   "tipo"
   end
 
   create_table "usuarios", :force => true do |t|
