4b5e9d68364a2a94ec4d769e4bc4dd6b31af8575
luischipa <l.chipa.e@gmail.com>
2012-08-11 02:24:39 -0500

    pagina pricipal

--- veterinaria/config/routes.rb
+++ veterinaria/config/routes.rb
@@ -3,8 +3,8 @@ get "log_out" => "sessions#destroy", :as => "log_out"
 get "log_in" => "sessions#new", :as => "log_in"
 get "sign_up" => "users#new", :as => "sign_up"
 
-#root :to => "users#new"
- 
+#root :to => "sessions#new"
+root :to => "patients#home"
 resources :users
 resources :sessions
 
@@ -20,7 +20,7 @@ resources :sessions
 
   resources :usuarios
 
-  root :to => "patients#home"
+  
   
   resources :histories
 
