1f861ee3d363741ad1927ab3ce3fecbbc0a331e3
danilo <denil206@gmail.com>
2012-08-11 12:19:56 +0200

    danilo

--- veterinaria/app/models/doctor.rb
+++ veterinaria/app/models/doctor.rb
@@ -1,3 +1,14 @@
 class Doctor < ActiveRecord::Base
 has_many :id_Prospectus
+
+	validates :name, :lastname, :phone, :email, :presence => {:in => true,
+		:message => " no puede estar vacio" }		
+	
+	validates :phone, :numericality => { :only_integer => true,
+		:message => " :Solo Numeros" }
+	
+	validates :email,   
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i ,
+			:message => " :Formato Incorrecto"}
 end
--- veterinaria/app/models/prospectu.rb
+++ veterinaria/app/models/prospectu.rb
@@ -1,3 +1,21 @@
 class Prospectu < ActiveRecord::Base
-has_attached_file :avatar
+	has_attached_file :avatar
+
+	validates :codigo, :name, :address, :phonecel, :presence => {:in => true,
+		:message => " no puede estar vacio" }
+		
+	validates :codigo, :length => { :is => 8, 
+		:message => " :Maximo solo 8 Numeros" }
+	
+	validates :codigo, :phonecel, :numericality => { :only_integer => true,
+		:message => " :Solo Numeros" }
+	
+	validates :codigo, :uniqueness => { :case_sensitive => true,
+		:message => " ya existe"}
+		
+	validates :email,               
+            :uniqueness => true,   
+            :format => { :with => /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/i ,
+			:message => " :Formato Incorrecto"}
+		
 end
